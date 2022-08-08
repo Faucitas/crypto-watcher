52eac5f2dd70ff2003c4fd8e7c5176b35db696e9
Tommy Smith <53862820+Faucitas@users.noreply.github.com>
2022-08-08 12:04:57 -0400

    update message.py

---
+++ message.py
@@ -0,0 +1,17 @@
+from twilio.rest import Client
+dotenv.load_dotenv()
+
+TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+TWILIO_MESSAGE_SID = os.environ['TWILIO_MESSAGE_SID']
+TWILIO_SENDING_NUMBER = os.environ['MY_PHONE_NUMBER']
+
+
+def send_text_message(message):
+    # Send a message with the percentage
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    message = client.messages.create(
+        messaging_service_sid=TWILIO_MESSAGE_SID,
+        body=message,
+        to=TWILIO_SENDING_NUMBER
+    )
